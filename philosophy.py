d7bbae0377be747313c08c4320ce3684a3982048
Restyled.io <commits@restyled.io>
2021-03-16 06:28:52 +0000

    Restyled by autopep8

--- philosophy.py
+++ philosophy.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
-import requests, re, time
+import requests
+import re
+import time
 
 philosophy_url = 'https://en.wikipedia.org/wiki/Philosophy'
 current_url = ""
@@ -7,18 +9,25 @@ visited_articles = []
 
 # function that removes all parentheses and all text between them
 # except in valid links, ex: 'https://en.wikipedia.org/wiki/Stimulus_(physiology)'
+
+
 def ignore_parentheses(text):
     no_parentheses_after_quoutes = re.sub('\"\((.*?)\)', '"', text)
-    no_parentheses_after_tags = re.sub('>\((.*?)\)', '>', no_parentheses_after_quoutes)
-    no_parentheses_after_curly_brackets = re.sub('}\((.*?)\)', '}', no_parentheses_after_tags)
-    no_parentheses_after_commas = re.sub(',\((.*?)\)', ',', no_parentheses_after_curly_brackets)
-    no_parentheses_after_periods = re.sub('\.\((.*?)\)', '.', no_parentheses_after_commas)
+    no_parentheses_after_tags = re.sub(
+        '>\((.*?)\)', '>', no_parentheses_after_quoutes)
+    no_parentheses_after_curly_brackets = re.sub(
+        '}\((.*?)\)', '}', no_parentheses_after_tags)
+    no_parentheses_after_commas = re.sub(
+        ',\((.*?)\)', ',', no_parentheses_after_curly_brackets)
+    no_parentheses_after_periods = re.sub(
+        '\.\((.*?)\)', '.', no_parentheses_after_commas)
     return re.sub(' \((.*?)\)', ' ', no_parentheses_after_periods)
+
 
 def get_to_philosophy(url):
 
     not_philosophy = True
-    
+
     while not_philosophy:
         time.sleep(0.5)
         response = requests.get(url)
@@ -41,10 +50,11 @@ def get_to_philosophy(url):
         # searches for wikipedia links inside of paragraphs, unordered lists, and ordered lists in article
         # and returns the url for the first valid link
         for text_element in text_elements:
-                if text_element.find('a', href=re.compile('/wiki/')) is not None:
-                    href = text_element.find('a', href=re.compile('/wiki/')).get('href')
-                    url = 'https://en.wikipedia.org' + href
-                    break
+            if text_element.find('a', href=re.compile('/wiki/')) is not None:
+                href = text_element.find(
+                    'a', href=re.compile('/wiki/')).get('href')
+                url = 'https://en.wikipedia.org' + href
+                break
 
         # checks for infinite loops and Dead-end pages
         if url == philosophy_url:
@@ -56,7 +66,8 @@ def get_to_philosophy(url):
             print(url)
             print('Loop detected')
             break
-            
+
+
 wikipedia_link = input()
 
 get_to_philosophy(wikipedia_link)
